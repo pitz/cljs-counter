26ce52ab87c03f2e80c04c879b1f8e9b28dc30e9
eduardo pitz <pitz.eduardo@gmail.com>
2022-10-16 10:41:56 -0300

    feat: Creating the app

---
+++ src/getting_started/counter.cljs
@@ -0,0 +1,47 @@
+(ns ^:figwheel-hooks getting-started.counter
+  (:require
+    [goog.dom :as gdom]
+    [goog.events :as gevents]))
+
+(defonce app-state
+  (atom 0))
+
+(def step-input
+  (gdom/getElement "step-input"))
+
+(def count-value
+  (gdom/getElement "count-value"))
+
+(def app-element
+  (gdom/getElement "app"))
+
+(defn get-step-input-value []
+  (js/Number (.. step-input -value)))
+
+(defn render-new-count [val]
+  (println " render-new-count :: val " val)
+  (gdom/setTextContent count-value val))
+
+(defn increase-count [app-state]
+  (swap! app-state inc))
+
+(defn decrease-count [app-state]
+  (swap! app-state dec))
+
+(defn step-count [app-state]
+  (swap! app-state + (get-step-input-value)))
+
+(defonce is-initialized?
+  (do
+   (gevents/listen app-element
+                   "click"
+                   (fn [event]
+                     (condp = (aget event "target" "id")
+                       "minus" (decrease-count app-state)
+                       "plus" (increase-count app-state)
+                       "step" (step-count app-state)
+                       nil)))
+   (add-watch app-state :counter-watcher
+              (fn [key atom old new]
+                (render-new-count new)))
+   true))
---
+++ test/getting_started/counter_test.cljs
@@ -0,0 +1,5 @@
+(ns getting-started.counter-test
+  (:require
+    [cljs.test :refer-macros [deftest is testing]]
+    [getting-started.counter :refer [multiply]]))
+
